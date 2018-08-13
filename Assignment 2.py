2cd61e7ee684421b7ba8c3493147c4a203c9644b
yochananlove1 <42241202+yochananlove1@users.noreply.github.com>
2018-08-13 16:55:15 +0100

    Add files via upload

---
+++ Assignment 2.py
@@ -0,0 +1,27 @@
+#this program is to perform a maths operation
+r = 14
+tita = 40
+sector = ((1/2) * r * r * tita)
+print ('the value for area of a sector =', sector)
+
+a = 20
+b = 10
+h = 30
+trapezoid = ((1/2) *(a+b) * h)
+print ('the value for area of a trapezoid =', trapezoid)
+
+l = 8
+square = l * l
+print ('the value for area of a square =', square)
+
+from math import pi
+r = 15
+h = 10
+cylinder = pi * r * r * h
+print ('the value for area of a cylinder =', cylinder)
+
+
+b = 50
+h = 20
+triangle = ((1/2) * b * h)
+print ('the value for area of a triangle =', triangle)
